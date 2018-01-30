721e2c24906dfc2c68c503a00e1cf2c4576676ee
roger2399 <roger.macwan@dal.ca>
2018-01-30 07:53:49 -0800

    test

---
+++ exercise_9.2.py
@@ -0,0 +1,16 @@
+######################################################
+#   EXRECISE 9.2
+#
+#
+#####################################################
+fin = open("words.txt")
+line = fin.readline()
+
+for line in fin:
+    #word = line.strip()
+        if line == 'e':
+            print (word)
+            #return true
+
+
+
